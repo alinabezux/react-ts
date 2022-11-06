0bd2e893f8ed014ae0e5a43abe07263d9d37c138
alinabezux <alinabezux@gmail.com>
2022-11-07 01:23:09 +0200

    react ts part1

---
+++ src/services/axios.service.ts
@@ -0,0 +1,9 @@
+import axios, {AxiosResponse} from "axios";
+
+import {baseURL} from "../configs";
+
+export type AxiosRes<T> = Promise<AxiosResponse<T>>
+
+const axiosService = axios.create({baseURL})
+
+export {axiosService}
